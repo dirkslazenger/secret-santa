f55000c00cac311d81a8ad2ba6ccb6fbb58d137f
dirkslazenger <ryanwatts21.2@gmail.com>
2023-12-17 19:31:43 +0000

    Change page title

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ function nanMode() {
     // If the checkbox is checked, display the output text
     if (checkBox.checked == true){
         document.getElementById('bg').style.backgroundImage="url(nan_version.png)";
-        document.getElementById("nanToggle").innerHTML = "Merry Christmas Nan"
     } else {
         document.getElementById('bg').style.backgroundImage="url(normal_version.png)";
-        document.getElementById("nanToggle").innerHTML = "Merry Christmas Maureen"
     }
   }
