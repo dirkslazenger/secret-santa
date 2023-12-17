5e0d7490697d30b6ebafcee734a3e3eb7c16741e
dirkslazenger <ryanwatts21.2@gmail.com>
2023-12-17 19:01:07 +0000

    Fix responsiveness

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function nanMode() {
     // If the checkbox is checked, display the output text
     if (checkBox.checked == true){
         document.getElementById('bg').style.backgroundImage="url(nan_version.png)";
+        document.getElementById("nanToggle").innerHTML = "Merry Christmas Nan"
     } else {
         document.getElementById('bg').style.backgroundImage="url(normal_version.png)";
+        document.getElementById("nanToggle").innerHTML = "Merry Christmas Maureen"
     }
   }
